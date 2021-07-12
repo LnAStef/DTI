adfbc97e59b3e198435c94c10d3816a08847da99
LnAStef <63543692+LnAStef@users.noreply.github.com>
2021-07-12 20:57:36 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 pip install -r requirements.txt #
-python openbiolink_ampligraph.py -epochs 250 -save-dir "./drive/MyDrive/WBS/complex_model_OpenBioLink.pkl" -output "./drive/MyDrive/WBS/complex_model_output.npy"
+python openbiolink_ampligraph.py -epochs 50 -save-dir "./drive/MyDrive/WBS/complex_model_OpenBioLink.pkl" -output "./drive/MyDrive/WBS/complex_model_output.npy"
